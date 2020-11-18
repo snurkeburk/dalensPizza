6dcc1c0f43dafdbff9748a042ef82c810454fbfe
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2020-11-18 15:04:21 +0100

    added "copy number to clipboard" function, added javascript to make copy and; added countdown to closing hour

---
+++ main.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var copyText = document.getElementById("nummer");
+  
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*Mobil*/
+  
+    document.execCommand("copy");
+  
+    alert("Kopierat!");
+  } 
+
+var d = new Date();
+var n = d.getDay();
+var deadline = new Date("Jan 5, 2222 21:00:00").getTime();
+var x = setInterval(function() { 
+    var now = new Date().getTime(); 
+    var t = deadline - now; 
+    var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+    var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+    document.getElementById("timer").innerHTML = 
+     hours + "h " + minutes + "m " + seconds + "s "; 
+        if (seconds <= 10) { 
+            clearInterval(x); 
+            document.getElementById("timer").innerHTML = "STÄNGT!"; 
+        }   
+    }, 1000); 
